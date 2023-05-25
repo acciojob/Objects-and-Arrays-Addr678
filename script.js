2a2d1d6c04f996fdc395ed6a497b0844cd98b305
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 09:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = [...players];
+const cap1 = { ...person };
+console.log(team);   
+console.log(team1);  
+console.log(cap1);
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
